faf551a2ef9415f0e87fc5797bb644f0d5956e49
Muhammad Bayu Oktavianto <metalkoholic@gmail.com>
2018-09-12 17:22:55 +0700

    add config

--- lib/heroku-deflater/railtie.rb
+++ lib/heroku-deflater/railtie.rb
@@ -5,6 +5,8 @@ require 'heroku-deflater/cache_control_manager'
 
 module HerokuDeflater
   class Railtie < Rails::Railtie
+    config.heroku_deflater = ActiveSupport::OrderedOptions.new
+
     initializer 'heroku_deflater.configure_rails_initialization' do |app|
       app.middleware.insert_before ActionDispatch::Static, Rack::Deflater
       app.middleware.insert_before ActionDispatch::Static, HerokuDeflater::SkipBinary
@@ -16,7 +18,7 @@ module HerokuDeflater
     # The configuration block in config/application.rb overrides this.
     config.before_initialize do |app|
       cache_control = cache_control_manager(app)
-      cache_control.setup_max_age(86400)
+      cache_control.setup_max_age(app.config.heroku_deflater[:age] || 86400)
     end
 
     def self.cache_control_manager(app)
